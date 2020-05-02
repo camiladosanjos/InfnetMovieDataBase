d6cceef0bcd0c936630177f6da071c4453d71423
Camila Vargas <camila.araujodosanjos@gmail.com>
2020-05-02 20:44:23 -0300

    inicial

---
+++ InfnetMovieDataBase/Controllers/FilmeController.cs
@@ -0,0 +1,101 @@
+﻿using InfnetMovieDataBase.Domain;
+using InfnetMovieDataBase.Repository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InfnetMovieDataBase.Controllers
+{
+    public class FilmeController : Controller
+    {
+        FilmeRepository repository = new FilmeRepository();
+        // GET: Filme
+        public ActionResult Index()
+        {
+            var filmes = repository.ListarFilmes();
+            return View(filmes);
+        }
+
+        // GET: Filme/Details/5
+        public ActionResult Details(int id)
+        {
+            var filmes = repository.DetalharFilme(id);
+
+            return View(filmes);
+        }
+
+        // GET: Filme/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Filme/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Filme filme)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                if (ModelState.IsValid)
+                {
+                    repository.CriarFilme(filme);
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View(filme);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Filme/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var filme = repository.DetalharFilme(id);
+
+            return View(filme);
+        }
+
+        // POST: Filme/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Filme filme)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    repository.AtualizarFilme(filme);
+
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View(filme);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Filme/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var filme = repository.DetalharFilme(id);
+
+            return View(filme);
+        }
+
+        // POST: Filme/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(Filme filme)
+        {
+            repository.ExcluirFilme(filme.Id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ InfnetMovieDataBase/Repository/FilmeRepository.cs
@@ -0,0 +1,175 @@
+﻿using InfnetMovieDataBase.Domain;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace InfnetMovieDataBase.Repository
+{
+    public class FilmeRepository
+    {
+        string connectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=InfnetMovieDataBase;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        //Listar Filmes
+        public IEnumerable<Filme> ListarFilmes()
+        {
+            var filmes = new List<Filme>();
+
+            using var connection = new SqlConnection(connectionString);
+
+            var cmdText = "SELECT * FROM Filme";
+            var select = new SqlCommand(cmdText, connection);
+
+            try
+            {
+                connection.Open();
+
+                using var reader = select.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (reader.Read())
+                {
+                    var filme = new Filme();
+
+                    filme.Id = (int)reader["Id"];
+                    filme.Titulo = reader["Titulo"].ToString();
+                    filme.TituloOriginal = reader["TituloOriginal"].ToString();
+                    filme.Ano = (int)reader["Ano"];
+
+                    filmes.Add(filme);
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            finally
+            {
+
+            }
+
+            return filmes;
+        }
+
+        //Criar Filme
+        public void CriarFilme(Filme filme)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var cmdText = "INSERT INTO Filme (Titulo, TituloOriginal, Ano) VALUES (@Titulo, @TituloOriginal, @Ano)";
+            var insert = new SqlCommand(cmdText, connection);
+
+            insert.CommandType = CommandType.Text;
+
+            insert.Parameters.AddWithValue("@Titulo", filme.Titulo);
+            insert.Parameters.AddWithValue("@TituloOriginal", filme.TituloOriginal);
+            insert.Parameters.AddWithValue("@Ano", filme.Ano);
+
+            try
+            {
+                connection.Open();
+                insert.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            finally
+            {
+
+            }
+        }
+
+        //Atualizar Filme
+        public void AtualizarFilme(Filme filme)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var cmdText = "UPDATE Filme SET Titulo=@Titulo, TituloOriginal=@TituloOriginal, Ano=@Ano WHERE Id=@Id";
+            var update = new SqlCommand(cmdText, connection);
+
+            update.Parameters.AddWithValue("@Id", filme.Id);
+            update.Parameters.AddWithValue("@Titulo", filme.Titulo);
+            update.Parameters.AddWithValue("@TituloOriginal", filme.TituloOriginal);
+            update.Parameters.AddWithValue("@Ano", filme.Ano);
+
+            try
+            {
+                connection.Open();
+                update.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            finally
+            {
+
+            }
+        }
+
+        //Detalhamento 
+        public Filme DetalharFilme(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+            string sql = "SELECT Id, Titulo, TituloOriginal, Ano FROM Filme WHERE Id=@Id";
+            SqlCommand cmd = new SqlCommand(sql, connection);
+            cmd.Parameters.AddWithValue("@Id", id);
+            Filme filme = null;
+
+            try
+            {
+                connection.Open();
+
+                using var reader = cmd.ExecuteReader(CommandBehavior.CloseConnection);
+
+                while (reader.Read())
+                {
+                    filme = new Filme();
+                    filme.Id = (int)reader["Id"];
+                    filme.Titulo = reader["Titulo"].ToString();
+                    filme.TituloOriginal = reader["TituloOriginal"].ToString();
+                    filme.Ano = (int)reader["Ano"];
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            finally
+            {
+
+            }
+
+            return filme;
+
+        }
+
+        //Excluir
+        public void ExcluirFilme(int id)
+        {
+            using var connection = new SqlConnection(connectionString);
+
+            var cmdText = "DELETE Filme WHERE Id=@Id";
+            var delete = new SqlCommand(cmdText, connection);
+
+            delete.Parameters.AddWithValue("@Id", id);
+           
+            try
+            {
+                connection.Open();
+                delete.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                throw (e);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
